95dfef552661eb767900a7c817fac0ba75508ce6
Ali Hamza Kamboh <123060177+ahkamboh@users.noreply.github.com>
2024-09-20 13:13:10 +0500

    CaptureText-cdn-test

--- cdns/CaptureText.js
+++ cdns/CaptureText.js
@@ -1,1 +1,45 @@
-class CaptureText{constructor(e,t,s=.7){this.textArray=e,this.selectors=t,this.speed=s,this.currentWordIndex=0,this.elements=document.querySelectorAll(this.selectors)}animateText(e){e.innerHTML=this.textArray[this.currentWordIndex].replace(/\S/g,"<span class='inline-block'>$&</span>"),anime.timeline({loop:!1}).add({targets:".animated-text .inline-block",scale:[4,1],opacity:[0,1],translateZ:0,easing:"easeOutExpo",duration:950/this.speed,delay:(e,t)=>70/this.speed*t}).add({targets:".animated-text .inline-block",opacity:0,duration:1e3,easing:"easeOutExpo",delay:1e3})}start(){let e=()=>{this.elements.forEach(e=>{this.animateText(e)}),this.currentWordIndex=(this.currentWordIndex+1)%this.textArray.length};e(),setInterval(()=>{e()},2750/this.speed)}}window.CaptureText=CaptureText;
+class CaptureText {
+    constructor(textArray, speed = 0.7) {
+      this.textArray = textArray;
+      this.speed = speed;
+      this.currentWordIndex = 0;
+      this.textElements = document.querySelectorAll('.CaptureText');
+      this.startTextAnimation();
+    }
+  
+    animateText(element) {
+      if (element) {
+        element.innerHTML = this.textArray[this.currentWordIndex].replace(
+          /\S/g,
+          "<span style='display: inline-block;'>$&</span>"
+        );
+  
+        anime.timeline({ loop: false })
+          .add({
+            targets: `.${element.className} span`,
+            scale: [4, 1],
+            opacity: [0, 1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 950 / this.speed,
+            delay: (el, i) => (70 / this.speed) * i,
+          })
+          .add({
+            targets: `.${element.className} span`,
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000,
+          });
+      }
+    }
+  
+    startTextAnimation() {
+      this.textElements.forEach(element => this.animateText(element));
+      setInterval(() => {
+        this.currentWordIndex = (this.currentWordIndex + 1) % this.textArray.length;
+        this.textElements.forEach(element => this.animateText(element));
+      }, 2750 / this.speed);
+    }
+  }
+  
